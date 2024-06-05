adabc7a85c153f67d30c8f1c46a8d5aaaf6a1416
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2024-06-04 22:20:54 -0400

    Player controller made

--- MarioClonePracticeBenitez/Assets/Code/PlayerController.cs
+++ MarioClonePracticeBenitez/Assets/Code/PlayerController.cs
@@ -5,4 +5,52 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //TO: Make player movement, jump and have a collider.
+
+    private float Horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform GroundCheck;
+    [SerializeField] private LayerMask GroundLayer;
+
+    //I wanna make some of the vars public so when I use power ups, they will be accessable and changed.
+    private void Update()
+    {
+        Horizontal = Input.GetAxisRaw("Horizontal");
+
+        if(Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(GroundCheck.position, 0.2f, GroundLayer);
+    }
+
+    private void Flip()
+    {
+        if(isFacingRight && Horizontal < 0f || !isFacingRight && Horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
 }
